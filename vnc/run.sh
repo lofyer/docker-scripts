98b9f54d1e761b4af3db88157cf5e6f481402bd1
lofyer <lofyer@gmail.com>
2020-05-28 10:32:17 +0800

    add wiznote

--- vnc/run.sh
+++ vnc/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it -e VNC_PW=password --user 0 -p 5901:5901 -p 6911:6901 consol/centos-xfce-vnc
+docker run -it -e VNC_PW=password --user 0 -p 5900-6900:5901 -p 6911:6901 consol/centos-xfce-vnc
 
 #docker run -it -p 6911:6901 \
 #    -e VNC_VIEW_ONLY=true \
