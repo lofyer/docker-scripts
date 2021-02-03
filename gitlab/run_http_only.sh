267e5bfda8ce7f7e062e000c3cc9ceec5e3a07fc
lofyer <lofyer@gmail.com>
2021-02-03 16:13:39 +0800

    update

--- gitlab/run_http_only.sh
+++ gitlab/run_http_only.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 docker run --detach \
     --hostname gitlab.lofyer.org \
-    --publish 8443:443 --publish 8080:80 --publish 8022:22 \
+    --publish 8080:80 --publish 8022:22 \
     -e GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.lofyer.org:8080/'; gitlab_rails['lfs_enabled'] = true;nginx['listen_port'] = 80;nginx['listen_https'] = false;" \
     --name gitlab \
     --restart always \
