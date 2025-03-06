1d593bed6c1b3b5af84f6fccb91ee76b567147c1
lofyer <lofyer@lofyerdeMac-mini-m4.local>
2025-03-06 10:56:02 +0800

    add iogpusetting

--- gpustack/get_compose_yml.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget https://github.com/gpustack/gpustack/raw/refs/heads/main/docker-compose.yaml -O docker-compose.yml
--- gpustack/start-server-worker-macos.sh
+++ gpustack/start-server-worker-macos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 curl -sfL https://get.gpustack.ai | INSTALL_PACKAGE_SPEC=gpustack[all] sh -s - --port 6080
+
+# Change service config file
 #/Library/LaunchDaemons/ai.gpustack.plist
 #sudo launchctl load /Library/LaunchDaemons/ai.gpustack.plist
 #sudo launchctl unload /Library/LaunchDaemons/ai.gpustack.plist
@@ -7,3 +9,6 @@ curl -sfL https://get.gpustack.ai | INSTALL_PACKAGE_SPEC=gpustack[all] sh -s - -
 #sudo launchctl stop ai.gpustack
 #sudo launchctl list | grep ai.gpustack
 
+# Maximize the macos gpu vmem, leave 4GB for the memory
+# sudo sysctl iogpu.wired_limit_mb=61440
+# sudo launchctl load /Library/LaunchDaemons/com.local.iogpusetting.plist
